ab1f0636e9ecaaf0aaac8e2ea1d7b3d0cc3fa808
Byte-Master63 <thokozanetek@gmail.com>
2024-11-01 14:54:32 -0700

    quick_sort

--- sort.h
+++ sort.h
@@ -14,7 +14,6 @@ typedef enum bool_e
 	true
 } bool;
 
-#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,18 +23,26 @@ typedef enum bool_e
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/* supplied functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-listint_t *swap_node(listint_t *node, listint_t **list);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+
+/* quick_sort() helpers */
+int partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+
+/* advanced tasks */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -44,5 +51,21 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+/* void sort_deck(deck_node_t **deck); */
 
-#endif
+/* cocktail_sort_list() helper */
+void dll_adj_swap(listint_t **list, listint_t *left, listint_t *right);
+
+/* merge_sort() helpers */
+void PrintArray(int *array, int iBeg, int iEnd);
+void CopyArray(int *array, int iBeg, int iEnd, int *work_copy);
+void TopDownMerge(int *array, int iBeg, int iMid, int iEnd, int *work_copy);
+void TopDownSplitMerge(int *work_copy, int iBeg, int iEnd, int *array);
+
+/* heap_sort() helpers */
+int iParent(int i);
+int iLeftChild(int i);
+void ConvertToHeap(int *array, size_t size);
+void SiftDown(int *array, size_t size, int start, int end);
+
+#endif /* SORT_H */
